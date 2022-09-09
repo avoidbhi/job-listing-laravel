57c77c1c56b516d939b4735f5cf88aa8237e15d5
Abhijeet Kanase <chllgoziecoder@outlook.com>
2022-09-09 18:41:43 +0530

    refactor(routes): code cleanup
    cleaning the web routes file
    
    Author: Abhijeet Kanase

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/post/{id}', function ($id) {
-    return response('Post .$id', ['id' => $id]);
-});
